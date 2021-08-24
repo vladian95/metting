5c026e0f5c55a3df7f4388725b179cc73d66142a
vladian95 <kvankvan1995@gmail.com>
2021-08-24 15:07:54 +0300

    project done

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,16 @@
 $(function(){
     $('.header__menu-btn').on('click', function(){
-        $('.header__menu ul').slideToggle();
-    });
-
-    
-
-
-    
+        $('.header__menu-list').slideToggle();
+    });   
 });
+
+
+const ratingItemsList = document.querySelectorAll('.rate-star');
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+
+ratingItemsArray.forEach(item => 
+    item.addEventListener('click', () => {
+        const { itemValue } = item.dataset;
+        item.parentNode.dataset.totalValue = itemValue;
+    })
+);
